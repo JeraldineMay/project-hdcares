11cbc99b3a82ab41448f0a70296e3a04935bb2dd
Jeraldin May <jeraldinemaytumulakvizarr@gmail.com>
2025-03-18 12:45:06 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-//npm install or npm i express
-
+// Load environment variables
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+// Middleware to parse JSON requests
+app.use(express.json());
 
 // Connect to the database
 const { connectDB } = require('./config/db');
 connectDB();
 
+// Import Routes
 const bhwRoutes = require('./routes/bhwRoutes');
 const rhuRoutes = require('./routes/rhuRoutes');
 const phoRoutes = require('./routes/phoRoutes');
